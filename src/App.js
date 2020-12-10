9300f1cf8158e067544cf4afd572674a887555e6
Julianb18 <julian141718@gmail.com>
2020-12-10 18:13:25 +0100

    alert messages

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { v4 as uuidv4 } from "uuid";
 
 import "./App.css";
 import { Recipe } from "./components/Recipe";
+import { Alert } from "./components/Alert";
 
 function App() {
   const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState([]);
+  const [alert, setAlert] = useState("");
 
   const APP_ID = "c6bfcd30";
 
@@ -16,10 +18,18 @@ function App() {
   const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
   const getData = async () => {
-    const result = await Axios.get(url);
-    setRecipes(result.data.hits);
-    console.log(result);
-    setQuery("");
+    if (query !== "") {
+      const result = await Axios.get(url);
+      if (!result.data.more) {
+        return setAlert("We can't seem to find that :(");
+      }
+      setRecipes(result.data.hits);
+      console.log(result);
+      setAlert("");
+      setQuery("");
+    } else {
+      setAlert("Please fill in the field");
+    }
   };
 
   const onChange = (e) => {
@@ -35,6 +45,7 @@ function App() {
     <div className="App">
       <h1>Food Searching App</h1>
       <form className="search-form" onSubmit={onSubmit}>
+        {alert !== "" && <Alert alert={alert} />}
         <input
           type="text"
           placeholder="Search Food..."
---
+++ src/components/Alert.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Alert = ({ alert }) => {
+  return (
+    <div className="alert">
+      <h3>{alert}</h3>
+    </div>
+  );
+};
